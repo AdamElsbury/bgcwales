a4ed7312bfbe79e374981bad638151b2e09427f8
adamelsbury <adam.john.elsbury@gmail.com>
2019-09-26 19:38:16 +0100

    Ready for internal server testing

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function showMobileMenu() {
+  var x = document.getElementById('menu-container');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
+}
+
+function closeMobileMenu() {
+  var x = document.getElementById('menu-container');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
