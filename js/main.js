fb258d0ac8eb4b7b1637a8610fc625e24dc40751
adamelsbury <adam.john.elsbury@gmail.com>
2019-10-09 15:42:57 +0100

    Added download instruction menu

--- js/main.js
+++ js/main.js
@@ -15,3 +15,23 @@ function closeMobileMenu() {
     x.style.display = 'block';
   }
 }
+
+// Download menu
+
+function showDownloadMenu() {
+  var x = document.getElementById('download-container');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
+}
+
+function closeDownloadMenu() {
+  var x = document.getElementById('download-container');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
